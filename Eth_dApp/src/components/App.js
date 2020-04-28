403f54ef5c6122ad1b7a6f4b121a0a50835684be
atibrewal98 <ankit@siplnet.com>
2020-04-28 19:29:33 +0530

    Clean Code

--- Eth_dApp/src/components/App.js
+++ Eth_dApp/src/components/App.js
@@ -174,51 +174,6 @@ async age(details,data){
     }
   }
 
-  async verifierEntity(verifierData){
-    //this.setState({loading: true});
-    const did = await this.state.identity.methods.identities(verifierData.publicKey).call();
-    this.setState({loading: false});
-    if(did.contentAddress) {
-      let data = await ipfs.get(did.contentAddress);
-      let d = JSON.parse(data[0].content.toString())
-      if(this.verify(d)) {
-        let data = JSON.parse(d.message);
-
-        var details = {};
-    details.publicKey = verifierData.publicKey;
-    if(verifierData.fn !== '')
-    details.fn = verifierData.fn;
-    
-    if(verifierData.ln !== '')
-    details.ln = verifierData.ln;
-    
-    if(verifierData.age !== '')
-    details.age = verifierData.age;
-    
-    if(verifierData.aadhar !== '')
-    details.aadhar = verifierData.aadhar;
-    
-    if(verifierData.mail !== '')
-    details.mail = verifierData.mail;
-    
-    if(verifierData.phone !== '')
-    details.phone = verifierData.phone;
-    console.log(data);
-
-    this.fn(details,data.data)
-    .then( () => {return this.ln(details,data.data)})
-    .then( () => {return this.mail(details,data.data)})
-    .then( () => {return this.phone(details,data.data)})
-    .then( () => {return this.aadhar(details,data.data)})
-    .then( () => {return this.age(details,data.data)})
-    .then( () => {  window.alert("Identity Verified Successfully...\n");})
-    .catch((error) => {window.alert(error)})
-      }
-    } else {
-      window.alert("Invalid Digital Signature");
-    }
-  }
-
   async addIPFS(res){
     if(this.verify(res)) {
 
@@ -251,7 +206,6 @@ async age(details,data){
 
     this.retrieveIdentity = this.retrieveIdentity.bind(this);
     this.addIPFS = this.addIPFS.bind(this);
-    this.verifierEntity = this.verifierEntity.bind(this);
   }
   
   render() {
@@ -270,7 +224,6 @@ async age(details,data){
                   : <Main
                     did={this.state.did}
                     retrieveIdentity={this.retrieveIdentity}
-                    verifierEntity={this.verifierEntity}
                     publicKey = {this.state.account}
                     addIPFS = {this.addIPFS}
                     />
